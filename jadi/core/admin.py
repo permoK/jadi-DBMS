478d47e9883077e71feb00c2178b88050cbc167d
MugumoPerm <perminusk21@gmail.com>
2024-05-25 01:23:13 +0300

    update model and add a button

--- jadi/core/admin.py
+++ jadi/core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 from .models import *
 
-
+admin.site.register(Institution)
 admin.site.register(UserProfile)
 admin.site.register(School)
 admin.site.register(Department)
