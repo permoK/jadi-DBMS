5879f6953b1a448c7f26eb4aea947fa9a302610f
permoK <perminusk21@gmail.com>
2024-06-06 18:33:52 +0300

    update

--- jadi/jadi/settings.py
+++ jadi/jadi/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    # Third party apps
+    'widget_tweaks',
     'django_extensions',
     'core',
 ]
